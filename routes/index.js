986a2ebf539f6a0eb2ee728cb3a3656315db3aa1
= <tsmit291@icloud.com>
2016-01-08 16:38:37 -0700

    html form is getting there

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var rest = "this is really cool";
 
 var knex = require('knex')({
   client: 'pg',
@@ -14,12 +13,25 @@ function restaurantinfo(){
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var allRows;
-  var tabley = knex.select().table('restaurantinfo').then(function (rows) {
+  var tabley = knex.select().table('restaurantinfo').then(function (rows){
     allRows = rows;
     res.render('restaurants/index', { obj: allRows });
   });
-  console.log(allRows);
-  console.log("****************");
 });
+
+router.get('/new', function(req, res, next){
+  var allRows;
+  var tabley = knex.select().table('restaurantinfo').then(function (rows){
+    allRows = rows;
+    res.render('restaurants/new', { obj: allRows });
+  });
+});
+
+// router.post('/restaurants', function(req, res, next){
+//   allRows = rows;
+//   restaurants().insert(restaurant).then(function(result){
+//     res.redirect('/restaurants');
+//   });
+// });
 
 module.exports = router;
