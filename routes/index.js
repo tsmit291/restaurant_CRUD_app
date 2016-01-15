fe5155926ecebc6be2b048f16f829b0c0b67e5f5
= <tsmit291@icloud.com>
2016-01-15 16:09:39 -0700

    heroku deployment

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var knex = require('knex')({
-  client: 'pg',
-  connection: 'postgres://localhost/restaurants'
-});
-
 function restaurantinfo(){
   return knex('restaurantinfo');
 };
