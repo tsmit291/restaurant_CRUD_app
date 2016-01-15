ec96aa2333482511dbb53cdfacc5fa4535c00932
= <tsmit291@icloud.com>
2016-01-15 16:16:14 -0700

    added dotenv

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require('dotenv').load();
 
 var routes = require('./routes/index');
 var admin = require('./routes/admin');
-
 var app = express();
 
 // view engine setup
